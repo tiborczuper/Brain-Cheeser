c8e3b75feda86bbb0ef3d297fc607eec44ba2a1b
Czuper Tibor <125367528+tiborczuper@users.noreply.github.com>
2025-09-26 11:14:33 +0200

    Update Brain Cheeser.bat

--- data/Brain Cheeser.bat
+++ data/Brain Cheeser.bat
@@ -1,4 +1,11 @@
 @echo off
 set PYTHONIOENCODING=utf-8
 title Brain Cheeser
+cd /d "%~dp0"
+
+echo Pygame ellenorzese es telepitese...
+python -c "import pygame" 2>nul || python -m pip install pygame
+
+echo Jatek inditasa...
 python main.py
+pause
