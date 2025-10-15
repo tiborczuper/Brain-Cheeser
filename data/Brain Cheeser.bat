bbe887902c92b4e74b1bf84a50f2a612d0ed29de
Czuper Tibor <125367528+tiborczuper@users.noreply.github.com>
2025-10-15 19:14:35 +0200

    fix

--- data/Brain Cheeser.bat
+++ data/Brain Cheeser.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFC9dTg2+GGS5E7gZ5vzo0/yU4k4SUOcDWp3a07rDA+gW71bhZ6op125bpMofHx5MbS6iYw4zrH1+v2eKOYmVsACB
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFC9dTg2+GGS5E7gZ5vzo0/yU4k4SUOcDWp3a07rDA+gW71bhZ6op125bpMofHx5MbS6iYw4zrH1+lHaEPsnckAfkT1uM9AsHEms6gnvV7A==
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 set PYTHONIOENCODING=utf-8
 title Brain Cheeser
